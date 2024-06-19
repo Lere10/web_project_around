02e1e57a147c51493207fd4eb79397d85d4da91b
Lere <leredsb10@gmail.com>
2024-06-19 19:15:40 -0300

    iteracao/populacao Feed

--- scripts/script.js
+++ scripts/script.js
@@ -31,3 +31,47 @@ function changeProfile(evt) {
 }
 
 submitButton.addEventListener("click", changeProfile);
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+function CriaPost(post) {
+  const feedTemplate = document.querySelector("#grid").content;
+  const feedPost = feedTemplate.querySelector(".grid__box").cloneNode(true);
+
+  const postImage = feedPost.querySelector(".grid__box-portrait-photo");
+  const postTitle = feedPost.querySelector(".grid__content-title");
+
+  postImage.src = post.link;
+  postTitle.textContent = post.name;
+
+  const feed = document.querySelector(".grid");
+  feed.prepend(feedPost);
+}
+initialCards.forEach((item) => {
+  CriaPost(item);
+});
