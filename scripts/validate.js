e3831e5c870e102666dc795dc05f6928427db83d
Lere <leredsb10@gmail.com>
2024-07-05 07:08:57 -0300

    fixed/BEMspans

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,15 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(
+    `.form__${inputElement.id}-error`
+  );
   inputElement.classList.add("form__input_error");
   errorElement.textContent = errorMessage;
 };
 
 const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  const errorElement = formElement.querySelector(
+    `.form__${inputElement.id}-error`
+  );
   inputElement.classList.remove("form__input_error");
   errorElement.textContent = "";
 };
